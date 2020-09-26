1f2329c9706a6155430bb13e7085d672da3450f8
aaa <1065537421@qq.com>
2020-09-26 18:28:56 +0800

    dist

---
+++ dist/static/js/acs.js
@@ -0,0 +1,22 @@
+import PubSub from 'pubsub-js'
+import axios from 'axios'
+import qs from 'qs'
+import {setCookie,getCookie} from '../../static/js/cookie.js'
+
+export function refresh_token(username, token) {
+    const url = `https://www.食.tech/acs/api/v1/auth/relogin`
+    let data = {
+        'username': username,
+        'token': token
+    }
+    axios.post(url, qs.stringify(data)).then(response => {
+        const refresh_token = response.data
+        console.log(refresh_token)
+        setCookie('lingxi-token',refresh_token, 3600*24)
+        setCookie('username',username, 3600*24)
+    }).catch(error => {
+        console.log(error)
+        alert('刷新token失败')
+    })
+}
+  
