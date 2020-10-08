4404ba5a24c2009b02a6f4fb1e32abe065182190
aaa <1065537421@qq.com>
2020-10-08 23:40:38 +0800

    修改字体颜色

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -281,7 +281,7 @@
             shareChange (scope) {
 
                 if (this.username !== scope.row.author) {
-                    this.$message.error('您无操作权限！')
+                    this.$message.success('不是您的博客哦！')
                     return
                 }
 
@@ -395,7 +395,7 @@
                 // 根据title去重
                 for (let i=0; i<this.datas.length; i++) {
                     if (this.form.title === this.datas[i].title) {
-                        this.$message.error('当前页含有重复标题！请重新输入')
+                        this.$message.success('含有重复标题哦！')
                         this.form.newCreating = false
                         return
                     }
@@ -568,7 +568,7 @@
                     var ids = ''
                     for(let i=0; i<val.length; i++){
                         if (this.username !== val[i].author) {
-                            this.$message.error('当前勾选中含有其他人的作品！')
+                            this.$message.success('含有其他人的博客哦！')
                             return
                         }
                         ids = ids + val[i].id + ','
@@ -626,7 +626,7 @@
                     alert('id为空')
                     this.tableData.splice(scope.$index, 1)
                 }else if (this.username !== scope.row.author) {
-                    this.$message.error('您无操作权限！')
+                    this.$message.success('不是您的博客哦！')
                 }else{
                     this.$router.push({path: "/main/edit/" + scope.row.id})
                 }
@@ -637,7 +637,7 @@
                     alert('id为空')
                     this.tableData.splice(scope.$index, 1)
                 }else if (this.username !== scope.row.author) {
-                    this.$message.error('您无操作权限！')
+                    this.$message.success('不是您的博客哦！')
                 }else {
                     this.$confirm('此操作将永久删除该文件, 是否继续?', '提示', {
                         confirmButtonText: '确定',
